52e08eb2b606a355cf10affd1336c4b0474c1dc4
ZhangWei-KUMO <kanseefoil@gmail.com>
2021-06-14 12:18:29 +0800

    add

--- main.go
+++ main.go
@@ -3,10 +3,11 @@
 package gorobot
 
 import "fmt"
+
 //  name string中的string为输入值类型，后一个string为输出值类型
 func Hello(name string) string {
-    // %v 为fmt包打印输出字符串占位符
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    // :=符号位go特有的初始化声明变量赋值方式，go语言对于变量不需进行类型声明。
-    return message
+	// %v 为fmt包打印输出字符串占位符
+	message := fmt.Sprintf("Hi, %v. 欢迎来的边界!", name)
+	// :=符号位go特有的初始化声明变量赋值方式，go语言对于变量不需进行类型声明。
+	return message
 }
